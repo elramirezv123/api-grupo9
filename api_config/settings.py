86a6f2ac7b55c819ccef39162e691176fbbde4c6
Eleuterio Ramirez <elramirez@uc.cl>
2019-04-24 12:30:58 -0400

    api dir name changed to api_config :sparkles:

--- api_config/settings.py
+++ api_config/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'bodega',
 ]
 
 MIDDLEWARE = [
@@ -50,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'api.urls'
+ROOT_URLCONF = 'api_config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'api.wsgi.application'
+WSGI_APPLICATION = 'api_config.wsgi.application'
 
 
 # Database
