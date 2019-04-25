9409205b10685edc5a1ca2bbca65f52bf3bae0a9
Eleuterio Ramirez <elramirez@uc.cl>
2019-04-24 20:19:05 -0400

    Adding new routes

--- bodega/urls.py
+++ bodega/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.conf.urls import url
 
 from . import views
 
 urlpatterns = [
     path('inventario', views.inventario),
+    path('pedidos', views.new_pedido),
+    url(r'pedidos/(?P<almacenId>\D+)/',views.pedidos),
+    path('orders', views.orders),
 ]
--- bodega/views.py
+++ bodega/views.py
@@ -1,16 +1,38 @@
-from django.shortcuts import render
 from django.http import JsonResponse
-from .utils import hashQuery
-import requests
+from .helpers.functions import get_skus_with_stock
+from .constants import almacenes
 
-
-# Create your views here.
+# https://www.webforefront.com/django/accessurlparamstemplates.html
 
 def inventario(request):
+    # Este es un ejemplo, aún no está listo.
+    sku = request.GET.get('sku', '')
+    if sku:
+        pass
+    else:
+        pass
     response = {
         "name": "grupo9",
         "cantidad_productos": "",
         "inventario": ""
     }
 
-    return JsonResponse(response)
+    return JsonResponse(response, safe=False)
+
+
+def new_pedido(request):
+    # Debe ser metodo POST
+    if request.method == 'POST':
+        pass
+
+
+def pedidos(request, almacenId):
+    # Debe ser método PUT y UPDATE
+    if request.method == 'PUT':
+        pass
+    elif request.method == 'DELETE':
+        pass
+
+def orders(request):
+    pass
+
