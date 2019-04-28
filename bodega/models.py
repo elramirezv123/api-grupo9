3a18e38261edcf1870660ab5ad6258151e30e019
JOSE POBLETE <jnpoblete@uc.cl>
2019-04-27 23:29:11 -0400

    UPDATE MODELS 5

--- bodega/models.py
+++ bodega/models.py
@@ -3,7 +3,7 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 
-class Product(models.Model):
+class product(models.Model):
     sku = models.CharField(primary_key=True, max_length=255)
     Nombre = models.TextField()
     Descripcion = models.TextField()
@@ -14,7 +14,7 @@ class Product(models.Model):
         """A string representation of the model."""
         return self.Nombre
 
-class Ingredient(models.Model):
+class ingredient(models.Model):
     Nombre = models.TextField()
     slug = models.SlugField(unique=True, max_length=255)
     created_on = models.DateTimeField(auto_now_add=True)
@@ -23,7 +23,7 @@ class Ingredient(models.Model):
         """A string representation of the model."""
         return self.Nombre
 
-class Pedidos(models.Model):
+class pedidos(models.Model):
     almacen_destino_id = models.CharField(max_length=255)
     sku_id = models.CharField(max_length=255)
     cantidad = models.CharField(max_length=255)
