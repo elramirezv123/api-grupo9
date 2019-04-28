468a803615189bf459fa57a3b9fa6fbcf9252318
Andrés Mardones <agmardones@uc.cl>
2019-04-28 02:26:13 -0400

    migration for loading data of productos.json

--- bodega/models.py
+++ bodega/models.py
@@ -3,31 +3,42 @@ from django.template.defaultfilters import slugify
 
 # Create your models here.
 
-class product(models.Model):
-    sku = models.CharField(primary_key=True, max_length=255)
-    Nombre = models.TextField()
-    Descripcion = models.TextField()
+
+class Product(models.Model):
+    sku = models.IntegerField(primary_key=True)
+    name = models.TextField()
+    description = models.TextField()
+    price = models.IntegerField(null=True)
+    slug = models.SlugField(unique=True, max_length=255)
+    duration = models.DecimalField(
+        null=True, decimal_places=2, max_digits=6)
+    equivalence = models.DecimalField(
+        null=True, decimal_places=3, max_digits=6)
+    unit = models.CharField(max_length=255, null=True)
+    batch = models.IntegerField(null=True)
+    production_time = models.IntegerField(null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """A string representation of the model."""
+        return self.name
+
+
+class Ingredient(models.Model):
+    name = models.TextField()
     slug = models.SlugField(unique=True, max_length=255)
     created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         """A string representation of the model."""
-        return self.Nombre
+        return self.name
 
-class ingredient(models.Model):
-    Nombre = models.TextField()
-    slug = models.SlugField(unique=True, max_length=255)
-    created_on = models.DateTimeField(auto_now_add=True)
 
-    def __str__(self):
-        """A string representation of the model."""
-        return self.Nombre
-
-class pedidos(models.Model):
-    almacen_destino_id = models.CharField(max_length=255)
+class Request(models.Model):
+    store_destination_id = models.CharField(max_length=255)
     sku_id = models.CharField(max_length=255)
-    cantidad = models.CharField(max_length=255)
+    amount = models.CharField(max_length=255)
 
     def __str__(self):
         """A string representation of the model."""
-        return str(self.sku_id) + str(self.cantidad)
+        return str(self.sku_id) + str(self.amount)
