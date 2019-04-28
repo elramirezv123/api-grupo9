48c757aa8c79d3a5c068fae06a7a1ac3bdad9e29
Andrés Mardones <agmardones@uc.cl>
2019-04-28 04:24:39 -0400

    requests post method creating request entity

--- bodega/helpers/functions.py
+++ bodega/helpers/functions.py
@@ -1,41 +1,49 @@
 from .utils import hashQuery
 from ..constants import apiKey, almacenes, apiURL, headers
 import requests
+import json
 
 '''
 Estas son funciones útiles para hacer las llamadas a la API del profe.
 '''
 
-# Funcions útiles para bodega 
+# Funcions útiles para bodega
+
+
 def get_skus_with_stock(almacenId):
     # Esta funcion permite obtener todos los sku no vencidos de algún almacén
     hash = hashQuery("GET"+almacenId)
-    headers["Authorization"]='INTEGRACION grupo9:{}'.format(hash)
-    response = requests.get(apiURL + "skusWithStock?almacenId={}".format(almacenId), headers=headers)
+    headers["Authorization"] = 'INTEGRACION grupo9:{}'.format(hash)
+    response = requests.get(
+        apiURL + "skusWithStock?almacenId={}".format(almacenId), headers=headers)
     return response
-    
+
 
 def get_products_with_sku(almacenId, sku):
     # Esta función permite obtener los primeros 100 productos (default) no vencidos
     # de algún almacén con algun sku.
     hash = hashQuery("GET"+almacenId+sku)
-    headers["Authorization"]='INTEGRACION grupo9:{}'.format(hash)
-    response = requests.get(apiURL + "skusWithStock?almacenId={}&sku={}".format(almacenId, sku), headers=headers)
+    headers["Authorization"] = 'INTEGRACION grupo9:{}'.format(hash)
+    response = requests.get(
+        apiURL + "skusWithStock?almacenId={}&sku={}".format(almacenId, sku), headers=headers)
     return response
+
 
 def send_product(productId, oc, address, price):
     # Despacha un producto no vencido presenta en la bodega de
     # despacho a la dirección indicada.
     hash = hashQuery("DELETE"+productId+address+price+oc)
-    headers["Authorization"]='INTEGRACION grupo9:{}'.format(hash)
-    body = {"productoId": productId, "oc": oc, "direccion": address, "precio": int(price)}
+    headers["Authorization"] = 'INTEGRACION grupo9:{}'.format(hash)
+    body = {"productoId": productId, "oc": oc,
+            "direccion": address, "precio": int(price)}
     response = requests.delete(apiURL + "stock", headers=headers, data=body)
     return response
+
 
 def move_product_inter_almacen(productId, almacenId):
     # Mueve un producto de un almacén a otro dentro de una misma bodega.
     hash = hashQuery("POST"+productId+almacenId)
-    headers["Authorization"]='INTEGRACION grupo9:{}'.format(hash)
+    headers["Authorization"] = 'INTEGRACION grupo9:{}'.format(hash)
     body = {"productoId": productId, "almacenId": almacenId}
     response = requests.post(apiURL + "moveStock", headers=headers, data=body)
 
@@ -45,9 +53,10 @@ def move_product_to_another_group(productId, almacenId):
     # a un almacén de recepcion de otro grupo.
     # En caso que almacén de recepción se encuentre lleno, los productos quedan en almacén pulmón.
     hash = hashQuery("POST"+productId+almacenId)
-    headers["Authorization"]='INTEGRACION grupo9:{}'.format(hash)
+    headers["Authorization"] = 'INTEGRACION grupo9:{}'.format(hash)
     body = {"productoId": productId, "almacenId": almacenId}
-    response = requests.post(apiURL + "moveStockBodega", headers=headers, data=body)
+    response = requests.post(apiURL + "moveStockBodega",
+                             headers=headers, data=body)
 
 
 # Funciones útiles para fábricar productos
@@ -58,18 +67,20 @@ def make_a_product(sku, quantity):
     # A través de este servicio se envían a fabricar productos con o sin materias primas.
     # Este servicio no requiere realizar el pago de la fabricación.
     hash = hashQuery("PUT"+sku+quantity)
-    headers["Authorization"]='INTEGRACION grupo9:{}'.format(hash)
+    headers["Authorization"] = 'INTEGRACION grupo9:{}'.format(hash)
     body = {"sku": str(sku), "cantidad": quantity}
-    response = request.put(apiURL + "fabrica/fabricarSinPago", headers=headers, data=body)
+    response = request.put(
+        apiURL + "fabrica/fabricarSinPago", headers=headers, data=body)
     return response
 
 # Funcions utiles para la recepción de productos
+
 
 def set_hook(url):
     # Setea la url para el hook de recepción de productos.
     # En caso de estar seteada anteriormente, se actualiza el valor.
     hash = hashQuery("PUT"+url)
-    headers["Authorization"]='INTEGRACION grupo9:{}'.format(hash)
+    headers["Authorization"] = 'INTEGRACION grupo9:{}'.format(hash)
     body = {"url": url}
     response = request.put(apiURL + "hook", headers=headers, data=body)
     return response
@@ -77,15 +88,18 @@ def set_hook(url):
 
 def get_hook():
     hash = hashQuery("GET")
-    headers["Authorization"]='INTEGRACION grupo9:{}'.format(hash)
+    headers["Authorization"] = 'INTEGRACION grupo9:{}'.format(hash)
     response = request.get(apiURL + "hook", headers=headers)
     return response
 
 
 def delete_hook(url):
     hash = hashQuery("DELETE")
-    headers["Authorization"]='INTEGRACION grupo9:{}'.format(hash)
+    headers["Authorization"] = 'INTEGRACION grupo9:{}'.format(hash)
     response = request.delete(apiURL + "hook", headers=headers)
     return response
 
 
+def get_request_body(request):
+    body_unicode = request.body.decode('utf-8')
+    return json.loads(body_unicode)
--- bodega/views.py
+++ bodega/views.py
@@ -1,12 +1,17 @@
 from django.http import JsonResponse
 from .helpers.functions import get_skus_with_stock
 from .constants import almacenes
+from .models import Request
+import json
+from django.views.decorators.csrf import csrf_exempt
+from .helpers.functions import get_request_body
 
 # https://www.webforefront.com/django/accessurlparamstemplates.html
 
 '''
 Estas son las vistas que representan los endpoints descritos en nuestra documentación.
 '''
+
 
 def inventario(request):
     # Este es un ejemplo, aún no está listo.
@@ -30,10 +35,11 @@ def new_pedido(request):
         pass
 
 
+@csrf_exempt
 def pedidos(request):
     # Debe ser método POST y UPDATE
     if request.method == 'POST':
-        #hay que guardar el pedido en la base de datos
+        # hay que guardar el pedido en la base de datos
         #
         # {
         # "almacen_destino_id": "5f7g13d5vwe3g6k423422423",
@@ -41,20 +47,10 @@ def pedidos(request):
         # "cantidad": 10
         # }
         #sku = request.POST.get('skuId', '')
-        cantidad = request.POST.get('cantidad', '')
         #almacen_id = request.POST.get('almacenDestinoId', '')
         # respuesta = check_products(sku, cantidad)
-        print(cantidad)
         # print(almacen_id)
         # print(cantidad)
-        respuesta = True
-        if respuesta:
-            response = {
-                "stock": 'true',
-              
-                "cantidad":cantidad,
-                
-            }   
         # else:
         #     #no hay suficiente stock
         #     response = {
@@ -62,21 +58,32 @@ def pedidos(request):
         #         "sku":sku,
         #         "cantidad":cantidad,
         #         "almacen_id":almacen_id,
-        #     }   
+        #     }
+        '''
+        Request usado:
+        {"store_destination_id": "asdasd", "sku_id": "012301", "amount": "10"}
+        '''
+        req_body = get_request_body(request)
+        request_entity = Request.objects.create(store_destination_id=req_body['store_destination_id'],
+                                                sku_id=req_body['sku_id'],
+                                                amount=req_body['amount'])
+
+        request_entity.save()
+        response = JsonResponse({'id': request_entity.id}, safe=False)
     elif request.method == 'DELETE':
         pass
     elif request.method == 'GET':
-        #pense en obtener el status de un pedido si es que el put y el repsonse con los productos son asincronos
-        #es decir, hago el pedido y se demoran un tiempo en entregar
-        #con el get chequeo el status del pedido
-       pass
-    return JsonResponse(response, safe=False)
-
-
+        # pense en obtener el status de un pedido si es que el put y el repsonse con los productos son asincronos
+        # es decir, hago el pedido y se demoran un tiempo en entregar
+        # con el get chequeo el status del pedido
+        pass
+    else:
+        response = JsonResponse({'error': {'type': 'MethodError'}}, safe=False)
+    return response
 
 
 def check_products(sku, cantidad):
-    #chequeo si hay stock del producto
+    # chequeo si hay stock del producto
     print(sku)
     if sku == '1':
         return True
