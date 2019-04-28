2bc84059541f5c40467075c96b40abe5dfeae8a1
JOSE POBLETE <jnpoblete@uc.cl>
2019-04-28 00:52:20 -0400

    UPDATE PEDIDOS

--- bodega/urls.py
+++ bodega/urls.py
@@ -9,7 +9,6 @@ a nuestra API. La idea es hacerlo igual a como est√° descrito en la documentaci√
 
 urlpatterns = [
     path('inventario', views.inventario),
-    path('pedidos', views.new_pedido),
-    url(r'pedidos/(?P<almacenId>\D+)',views.pedidos),
-    # path('orders', views.orders),
+    path('pedidos', views.pedidos),
+    url(r'pedidos/(?P<cantidad>\D+)',views.pedidos),
 ]
--- bodega/views.py
+++ bodega/views.py
@@ -30,10 +30,55 @@ def new_pedido(request):
         pass
 
 
-def pedidos(request, almacenId):
-    # Debe ser método PUT y UPDATE
-    if request.method == 'PUT':
-        pass
+def pedidos(request):
+    # Debe ser método POST y UPDATE
+    if request.method == 'POST':
+        #hay que guardar el pedido en la base de datos
+        #
+        # {
+        # "almacen_destino_id": "5f7g13d5vwe3g6k423422423",
+        # "sku_id": 80,
+        # "cantidad": 10
+        # }
+        #sku = request.POST.get('skuId', '')
+        cantidad = request.POST.get('cantidad', '')
+        #almacen_id = request.POST.get('almacenDestinoId', '')
+        # respuesta = check_products(sku, cantidad)
+        print(cantidad)
+        # print(almacen_id)
+        # print(cantidad)
+        respuesta = True
+        if respuesta:
+            response = {
+                "stock": 'true',
+              
+                "cantidad":cantidad,
+                
+            }   
+        # else:
+        #     #no hay suficiente stock
+        #     response = {
+        #         "stock": 'true',
+        #         "sku":sku,
+        #         "cantidad":cantidad,
+        #         "almacen_id":almacen_id,
+        #     }   
     elif request.method == 'DELETE':
         pass
+    elif request.method == 'GET':
+        #pense en obtener el status de un pedido si es que el put y el repsonse con los productos son asincronos
+        #es decir, hago el pedido y se demoran un tiempo en entregar
+        #con el get chequeo el status del pedido
+       pass
+    return JsonResponse(response, safe=False)
 
+
+
+
+def check_products(sku, cantidad):
+    #chequeo si hay stock del producto
+    print(sku)
+    if sku == '1':
+        return True
+    else:
+        return False
