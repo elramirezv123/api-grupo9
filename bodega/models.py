d7e2ce847cb285711c595579ea001e90909a06fc
JOSE POBLETE <jnpoblete@uc.cl>
2019-04-27 20:14:42 -0400

    UPDATE MODELS 3

--- bodega/models.py
+++ bodega/models.py
@@ -15,3 +15,9 @@ class Ingredient(models.Model):
     Nombre = models.TextField()
     slug = models.SlugField(unique=True, max_length=255)
     created_on = models.DateTimeField(auto_now_add=True)
+
+class Pedidos(models.Model):
+    id = models.CharField(primary_key=True, max_length=255, autoincrement=True)
+    almacen_destino_id = models.CharField(max_length=255)
+    sku_id = models.CharField(max_length=255)
+    cantidad = models.CharField(max_length=255)
