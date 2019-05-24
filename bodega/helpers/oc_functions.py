b1a2aa5343711dd95a3b4a131d5a305ef279d2c6
Eleuterio Ramirez <elramirez@uc.cl>
2019-05-24 12:05:45 -0400

    Adding new functions

---
+++ bodega/helpers/oc_functions.py
@@ -0,0 +1,44 @@
+from .utils import hashQuery
+from ..constants import almacenes, almacen_stock, minimum_stock, prom_request, DELTA, sku_products, REQUEST_FACTOR
+from ..constants import ocURL, headers
+import requests
+import json
+
+def deleteOc(ocId, reason):
+    body = {"anulacion": reason}
+    response = requests.delete(ocURL + "anular/{}".format(ocId), headers=headers, json=body)
+    return response.json()
+
+def newOc(clientId, vendorId, sku, delivery_date, quantity, unitPrice, channel, *args):
+    body = {
+    "cliente": clientId,
+    "proveedor": vendorId,
+    "sku": sku,
+    "fechaEntrega": delivery_date,
+    "cantidad": quantity,
+    "precioUnitario": unitPrice,
+    "canal": channel,
+    "notas": args[0],
+    "urlNotificacion": args[1]
+    }
+    response = requests.put(ocURL + "crear", headers=headers, json=body)
+    return response.json()
+
+def getOc(ocId):
+    response = requests.get(ocURL + "obtener/{}".format(ocId), headers=headers)
+    return response.json()
+
+
+def receiveOc(ocId):
+    body = {"_id": ocId}
+    response = requests.post(ocURL + "recepcionar/{}".format(ocId), headers=headers)
+    return response.json()
+
+def deleteOc(ocId, reason):
+    body = {"rechazo": reason}
+    response = requests.post(ocURL + "rechazar/{}".format(ocId), headers=headers, json=body)
+    return response.json()
+
+
+
+
