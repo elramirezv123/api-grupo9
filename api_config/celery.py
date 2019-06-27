d666d67b1c697cda4a08f9507f34e78bfec895d3
swidemann <sowidemann@uc.cl>
2019-06-27 15:48:26 -0400

    celery: empty_pulmon and empty_recepcion_HTTPless

--- api_config/celery.py
+++ api_config/celery.py
@@ -38,6 +38,14 @@ app.conf.beat_schedule = {
         'task': 'create-mid-products',  
         'schedule': crontab(minute='*/20'), 
         'relative': True  
+    },
+    'empty-pulmon': {  
+        'task': 'empty-pulmon',  
+        'schedule': crontab(minute='*/40',   
+    },
+    'empty-recepcion-HTTPless': {  
+        'task': 'empty-recepcion-HTTPless',  
+        'schedule': crontab(minute='*/20'),   
     }
 }
 
